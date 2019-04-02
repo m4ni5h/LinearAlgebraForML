c2b877c93e62f79addd1dc88f019f82dcdb731a6
deeplearning <manish.kumar5139@gmail.com>
2019-04-02 21:43:24 +0530

    Linear Algebra Chapter 4 complete

--- JasonBrownLee_LinearAlgebra/LinearAlgebra.py
+++ JasonBrownLee_LinearAlgebra/LinearAlgebra.py
@@ -27,3 +27,31 @@ print(a)
 from numpy import ones
 a = ones([5])
 print(a)
+
+# create array with vstack
+from numpy import array
+from numpy import vstack
+# create first array
+a1 = array([1,2,3])
+print(a1)
+# create second array
+a2 = array([4,5,6])
+print(a2)
+# vertical stack
+a3 = vstack((a1, a2))
+print(a3)
+print(a3.shape)
+
+# create array with hstack
+from numpy import array
+from numpy import hstack
+# create first array
+a1 = array([1,2,3])
+print(a1)
+# create second array
+a2 = array([4,5,6])
+print(a2)
+# create horizontal stack
+a3 = hstack((a1, a2))
+print(a3)
+print(a3.shape)
