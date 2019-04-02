ca610e74192fc5ef0babf20c5452d4fc0ef646ae
deeplearning <manish.kumar5139@gmail.com>
2019-04-02 21:40:17 +0530

    Linear Algebra for Machine Learning

--- JasonBrownLee_LinearAlgebra/LinearAlgebra.py
+++ JasonBrownLee_LinearAlgebra/LinearAlgebra.py
@@ -1,2 +1,29 @@
 import numpy as np
 import pandas as pn
+
+# create array
+from numpy import array
+# create array
+l = [1.0, 2.0, 3.0]
+a = array(l)
+# display array
+print(a)
+# display array shape
+print(a.shape)
+# display array data type
+print(a.dtype)
+
+# create empty array
+from numpy import empty
+a = empty([3,3])
+print(a)
+
+# create zero array
+from numpy import zeros
+a = zeros([3,5])
+print(a)
+
+# create one array
+from numpy import ones
+a = ones([5])
+print(a)
