71742b55b5c58ff5faf96c4db014b3e227ce9d13
deeplearning <manish.kumar5139@gmail.com>
2019-04-03 06:52:23 +0530

    Numpy array slicling

--- JasonBrownLee_LinearAlgebra/LinearAlgebra.py
+++ JasonBrownLee_LinearAlgebra/LinearAlgebra.py
@@ -55,3 +55,149 @@ print(a2)
 a3 = hstack((a1, a2))
 print(a3)
 print(a3.shape)
+
+
+# 5 Index, Slice and Reshape NumPy Arrays
+# create one-dimensional array
+from numpy import array
+# list of data
+data = [11, 22, 33, 44, 55]
+# array of data
+data = array(data)
+print(data)
+print(type(data))
+# create two-dimensional array
+from numpy import array
+# list of data
+data = [[11, 22],
+[33, 44],
+[55, 66]]
+# array of data
+data = array(data)
+print(data)
+print(type(data))
+# index a one-dimensional array
+from numpy import array
+# define array
+data = array([11, 22, 33, 44, 55])
+# index data
+print(data[0])
+print(data[4])
+# index array out of bounds
+from numpy import array
+# define array
+data = array([11, 22, 33, 44, 55])
+# index data
+print(data[5])
+# negative array indexing
+from numpy import array
+# define array
+data = array([11, 22, 33, 44, 55])
+# index data
+print(data[-1])
+print(data[-5])
+# index two-dimensional array
+from numpy import array
+# define array
+data = array([
+[11, 22],
+[33, 44],
+[55, 66]])
+# index data
+print(data[0,0])
+# index row of two-dimensional array
+from numpy import array
+# define array
+data = array([
+[11, 22],
+[33, 44],
+[55, 66]])
+# index data
+print(data[0,])
+# slice a one-dimensional array
+from numpy import array
+# define array
+data = array([11, 22, 33, 44, 55])
+print(data[:])
+# slice a subset of a one-dimensional array
+from numpy import array
+# define array
+data = array([11, 22, 33, 44, 55])
+print(data[0:1])
+# negative slicing of a one-dimensional array
+from numpy import array
+# define array
+data = array([11, 22, 33, 44, 55])
+print(data[-2:])
+# split input and output data
+from numpy import array
+# define array
+data = array([
+[11, 22, 33],
+[44, 55, 66],
+[77, 88, 99]])
+# separate data
+X, y = data[:, :-1], data[:, -1]
+print(X)
+print(y)
+# split train and test data
+from numpy import array
+# define array
+data = array([
+[11, 22, 33],
+[44, 55, 66],
+[77, 88, 99]])
+# separate data
+split = 2
+train,test = data[:split,:],data[split:,:]
+print(train)
+print(test)
+
+# shape of one-dimensional array
+from numpy import array
+# define array
+data = array([11, 22, 33, 44, 55])
+print(data.shape)
+
+# shape of a two-dimensional array
+from numpy import array
+# list of data
+data = [[11, 22],
+[33, 44],
+[55, 66]]
+# array of data
+data = array(data)
+print(data.shape)
+
+# row and column shape of two-dimensional array
+from numpy import array
+# list of data
+data = [[11, 22],
+[33, 44],
+[55, 66]]
+# array of data
+data = array(data)
+print('Rows: %d' % data.shape[0])
+print('Cols: %d' % data.shape[1])
+
+# reshape 1D array to 2D
+from numpy import array
+# define array
+data = array([11, 22, 33, 44, 55])
+print(data.shape)
+# reshape
+data = data.reshape((data.shape[0], 1))
+print(data.shape)
+
+# reshape 2D array to 3D
+from numpy import array
+# list of data
+data = [[11, 22],
+[33, 44],
+[55, 66]]
+# array of data
+data = array(data)
+print(data.shape)
+# reshape
+data = data.reshape((data.shape[0], data.shape[1], 1))
+print(data.shape)
