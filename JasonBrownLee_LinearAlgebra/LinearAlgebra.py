a630af654c0814896c77a73c62dc59aadce18694
Manish Kumar <manish.kumar5139@gmail.com>
2019-04-24 10:40:28 +0530

    Chapter 9 code snippets
    Chapter 9 (Matrices and Matrix Arithmetic) code snippets

--- JasonBrownLee_LinearAlgebra/LinearAlgebra.py
+++ JasonBrownLee_LinearAlgebra/LinearAlgebra.py
@@ -364,3 +364,120 @@ print(a)
 maxnorm = norm(a, inf)
 print(maxnorm)
 
+# 9 Matrices and Matrix Arithmetic
+# create matrix
+from numpy import array
+A = array([[1, 2, 3], [4, 5, 6]])
+print(A)
+
+# matrix addition
+from numpy import array
+# define first matrix
+A = array([
+[1, 2, 3],
+[4, 5, 6]])
+print(A)
+# define second matrix
+B = array([
+[1, 2, 3],
+[4, 5, 6]])
+print(B)
+# add matrices
+C = A + B
+print(C)
+
+# matrix subtraction
+from numpy import array
+# define first matrix
+A = array([
+[1, 2, 3],
+[4, 5, 6]])
+print(A)
+# define second matrix
+B = array([
+[0.5, 0.5, 0.5],
+[0.5, 0.5, 0.5]])
+print(B)
+# subtract matrices
+C = A - B
+print(C)
+
+# matrix Hadamard product
+from numpy import array
+# define first matrix
+A = array([
+[1, 2, 3],
+[4, 5, 6]])
+print(A)
+# define second matrix
+B = array([
+[1, 2, 3],
+[4, 5, 6]])
+print(B)
+# multiply matrices
+C = A * B
+print(C)
+
+# matrix division
+from numpy import array
+# define first matrix
+A = array([
+[1, 2, 3],
+[4, 5, 6]])
+print(A)
+# define second matrix
+B = array([
+[1, 2, 3],
+[4, 5, 6]])
+print(B)
+# divide matrices
+C = A / B
+print(C)
+
+# matrix dot product
+from numpy import array
+# define first matrix
+A = array([
+[1, 2],
+[3, 4],
+[5, 6]])
+print(A)
+# define second matrix
+B = array([
+[1, 2],
+[3, 4]])
+print(B)
+# multiply matrices
+C = A.dot(B)
+print(C)
+# multiply matrices with @ operator
+D = A @ B
+print(D)
+
+# matrix-vector multiplication
+from numpy import array
+# define matrix
+A = array([
+[1, 2],
+[3, 4],
+[5, 6]])
+print(A)
+# define vector
+B = array([0.5, 0.5])
+print(B)
+# multiply
+C = A.dot(B)
+print(C)
+
+# matrix-scalar multiplication
+from numpy import array
+# define matrix
+A = array([[1, 2], [3, 4], [5, 6]])
+print(A)
+# define scalar
+b = 0.5
+print(b)
+# multiply
+C = A * b
+print(C)
+
